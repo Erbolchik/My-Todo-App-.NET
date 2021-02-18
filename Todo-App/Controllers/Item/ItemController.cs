0fe99a608b0d10d1b0f626e24abfb8221eebf9fc
erbol.daurenyli@gmail.com <erbol.daurenyli@gmail.com>
2021-02-18 23:53:37 +0600

    mock for item

---
+++ Todo-App/Controllers/Item/ItemController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Todo_App.Controllers.Item
+{
+    [Route("api/item")]
+    public class ItemController : ApiController
+    {
+        [HttpGet("{id}")]
+        public string GetItem()
+        {
+            return "hello world";
+        }
+
+        public void GetItems()
+        {
+
+        }
+
+        public void SaveItem()
+        {
+
+        }
+
+        public void UpdateItem()
+        {
+
+        }
+
+        public void DeleteItem()
+        {
+
+        }
+    }
+}
